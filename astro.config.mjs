a4bcd9ea6c07c1d01d0934a23460cf4a7987f1b5
Diego Rios <darp_0703@outlook.com>
2025-11-09 16:30:36 -0600

    feat: Add GitHub Actions workflow for deploying to GitHub Pages
    - Create a new workflow file to automate deployment to GitHub Pages on push to the master branch.
    - Set up necessary permissions and concurrency settings.
    - Define build and deploy jobs, including steps for checking out code, setting up dependencies, building the project, and deploying the artifact.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://dandresrp.github.io",
+  base: "/eatify-landing",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
